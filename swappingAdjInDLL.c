57b3634b7c7f90cf4d2395eaeedd3d7d4f6ec213
Sanish <sanishpagui162@gmail.com>
2024-08-06 22:25:12 +0530

    ez

--- swappingAdjInDLL.c
+++ swappingAdjInDLL.c
@@ -5,7 +5,7 @@ struct node{
     int info;
     struct node *next;
 };
-struct node *start;
+struct node *start=NULL;
 struct node *addAtBeg(int data){
     struct node *temp;
     temp=(struct node *)malloc(sizeof(struct node));
@@ -23,11 +23,11 @@ struct node *addAtEnd(int data){
     struct node *temp,*p=start;
     temp=(struct node *)malloc(sizeof(struct node));
     temp->info=data;
+    temp->next=NULL;
     while(p->next!=NULL)
         p=p->next;
     p->next=temp;
     temp->prev=p;
-    temp->next=NULL;
     return start;
 }
 struct node *createList(){
@@ -47,36 +47,67 @@ struct node *createList(){
     return start;
 }
 void display(){
-    struct node *p=start;
-    if(start==NULL){
-        printf("List is empty");
-        return ;
+    struct node *p;
+    printf(" ________    ");
+    for(p=start;p!=NULL;p=p->next)
+        printf(" ___ ________    ");
+    printf("\n|");
+    printf("%lx|-->|",start);
+    for(p=start;p!=NULL;p=p->next){
+        if(p->next==NULL){
+            printf("__%d|__NULL__|-->",p->info,p->next); 
+            break;
+        }
+        if(p->info>=0 && p->info<10)
+            printf("__%d|%lx|-->|",p->info,p->next); 
+        else if(p->info>=10 && p->info<100)
+            printf("_%d|%lx|-->|",p->info,p->next); 
+        else
+            printf("%d|%lx|-->|",p->info,p->next);
     }
+    printf("NULL\n");
+    int i=0;
+    while(i<13){
+        printf(" ");
+        i++;
+    }
+    p=start;
     while(p!=NULL){
-        printf("%d\t",p->info);
+        printf("   ");
+        printf("%lx",p);
+        printf("      ");
         p=p->next;
     }
     printf("\n");
 }
 void swapping(){
-    struct node *p=start;
-    int temp;
-    if(p->next==NULL){
-        printf("Only one element");
-        return;
+    struct node *p=start,*prev=NULL,*q=start->next;
+    while(q!=NULL){
+        p->next=q->next;
+        if(p->next!=NULL)
+            p->next->prev=p;
+        q->prev=prev;
+        q->next=p;
+        p->prev=q;
+        if(prev==NULL)
+            start=q;
+        else
+            prev->next=q;
+        prev=p;
+        p=p->next;
+        if(p!=NULL)
+            q=p->next;
+        else
+            q=NULL;
     }
-    while(p!=NULL){
-        temp=p->info;
-        p->info=p->next->info;
-        p->next->info=temp;
-        p=p->next->next;
-    }
-    display();
+    if(p!=NULL)
+        p->next=NULL;
 }
 int main(){
     createList();
-    pritnf("The Linked list before swapping\n");
+    printf("The Linked list before swapping\n");
     display();
-    pritnf("The Linked list after swapping\n");
+    printf("The Linked list after swapping\n");
     swapping();
+    display();
 }
